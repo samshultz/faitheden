807f5fa095920a502869d72158ba214408c7a55c
Samuel Taiwo <taiwogabrielsamuel@gmail.com>
2017-10-24 23:12:45 +0100

    made it better

--- about/models.py
+++ about/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class AboutUs(models.Model):
-    title = models.CharField(max_length=200, default="About Us")
+    title = models.CharField(max_length=200, blank=True)
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
