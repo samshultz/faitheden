c5da4d50c11bcbe4ce7e176c174f7aeceb381987
Samuel Taiwo <taiwogabrielsamuel@gmail.com>
2017-10-25 06:49:16 +0100

    made it better

--- about/views.py
+++ about/views.py
@@ -3,6 +3,6 @@ from .models import AboutUs
 
 def About(request):
     # Get the first about us object
-    about_us = AboutUs.objects.all()[0]
+    about_us = AboutUs.objects.first()
     context = {'about_us': about_us}
     return render(request, 'about/about.html', context)
