5fd656aa269bf0acf9b30b94a6bebf1bb1575f3e
Samuel Taiwo <taiwogabrielsamuel@gmail.com>
2017-11-04 22:48:44 +0100

    updated the media root

--- myproject/settings.py
+++ myproject/settings.py
@@ -144,7 +144,7 @@ STATICFILES_DIRS = [
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join ((BASE_DIR), 'media/')
+MEDIA_ROOT = os.path.join ((PROJECT_ROOT), 'media/')
 
 
 CRISPY_TEMPLATE_PACK='bootstrap3'
